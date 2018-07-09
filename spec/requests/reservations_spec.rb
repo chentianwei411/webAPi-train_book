8f3e3e51e723decc9b4f553ed5259930a77d0aab
ctw <ctw.3@163.com>
2018-07-09 17:51:59 +0800

    practice

---
+++ spec/requests/reservations_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Reservations", type: :request do
+  describe "GET /reservations" do
+    it "works! (now write some real specs)" do
+      get reservations_index_path
+      expect(response).to have_http_status(200)
+
+      dd
+    end
+  end
+end
